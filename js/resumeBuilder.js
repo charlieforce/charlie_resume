bac447e3169db2258f90728f83e0d6435bb9dfc8
CharlieKB <charlieforce1999@gmail.com>
2017-08-28 17:23:02 -0700

    Resme for the Udacity course
    This is a simple resume for a course with Udacity

---
+++ js/resumeBuilder.js
@@ -0,0 +1,428 @@
+var bio = {
+  "name": "Charlie Bond Kebbi",
+  "role": "Junior Software Developer",
+  "contacts": {
+    "mobile": '604-721-0589',
+    "LinkedIN": "CharlieKB",
+    "email": "charlieforce1999@gmail.com",
+    "github": "charlieforce",
+    "location": "Vancouver, BC, Canada"
+  },
+  "biopic": "images/chuck.jpg",
+  "welcomeMessage": "I'm currently working as a Junior Software Developer in Vancouver, BC, Canada.  Thanks for stopping by!",
+  "skills": ["JavaScript", "Ruby Sinatra", "Software Development", "Ruby", "Test Driven Development", "Product Management", "FrontEnd Development"]
+};
+
+var work = {
+  "jobs": [{
+    "employer": "BragOut LLC",
+    "title": "Co-Founder and Product Manager",
+    "location": "Vancouver, BC, Canada",
+    "dates": "January 2015 - Present",
+    "description": "Co-Founded a social media website for sports fans and enthusiasts. Bragout was developed to solve one major problem. Many sports Enthusiasts needed a place to brag, promote and talk about what is of interest to them. The entire experience is best on one's passion. All connections are based on a single passion. BragOut LLC focuses on engaging clients across the globe through an engaging and innovative social networking site that connects Sports and Physical activity Enthusiasts based on their individual passion with each other. All connections are based on a particular passion in a clutter-free environment. You will never read stories, or meet users that have a different passion. This is make possible for users to market themselves by creating a bio."
+  }, {
+    "employer": "Promoga",
+    "title": "Product Manager",
+    "location": "Vancouver, BC, Canada",
+    "dates": "March 2015 - March 2016",
+    "description": "My primary responsibilities involve all aspects of developing and supporting the development and marketing groups. I worked with the designers to design the UX/UI views. Worked with the marketing team and the developers to test our assumptions and bing our product to the market. Promoga is a social fitness and wellness community marketplace that connects independent instructors to students and available workout spaces, and facilitates socializing after class to build social connections. It is free for instructors to use and offers tools to promote classes and direct deposit of student payments. Promoga uses a social capitalism model - we're part of the sharing economy."
+  }, {
+    "employer": "TechieVet",
+    "title": "Co-Founder, UX/UI Designer",
+    "location": "Portland, OR",
+    "dates": "September 2014 - December 2015",
+    "description": "Worked as the UX/UI designer for the TechieVet's website. Techievet is a web design and web development consulting company based in Portland, Oregon. The company help US military Veterans with business ideas in Tech, build their startups. The focused primaryly on tech companies with founders who had no technical backgrounds."
+  }, {
+    "employer": "Servsco",
+    "title": "Co-Founder, Product Manager",
+    "location": "Portland, OR",
+    "dates": "March 20013 - September 2015",
+    "description": "Managed and developed a tool for anyone to look for services in their neighbourhood. I was responsbile for all phases of the product."
+  }, {
+    "employer": "US Government",
+    "title": "Project Manager",
+    "location": "Portland, OR",
+    "dates": "September 2011 - April 20015",
+    "description": "Managed a group of 5 developers and 2 UX/UI experts. Our main function was in building mobile and web applications for US Federal agencies. We thouht through all pojects and developed prototypes before building the products. My responsibilities included, mentoring junior developers, managing the schedule, budget and plan. Meeting with stakeholders and customers and making sure we were on track."
+  }, {
+    "employer": "SFU",
+    "title": "Equity Analyst",
+    "location": "Vancouver, BC",
+    "dates": "May 2009 - August 2011",
+    "description": "Managed a group of Analysts from the Simon Fraser University's Msc in Finance program. We managed the schools investment funds. We were responsible for researching new fuds and either buying or recommending which funds should be added or sold from our portfolio."
+  }, {
+    "employer": "Talecris",
+    "title": "Operations Manager",
+    "location": "Seattle, WA",
+    "dates": "May 2004 - August 2009",
+    "description": "Managed the Talecris lab in Seattle and Tacoma, WA."
+  }, {
+    "employer": "US Army",
+    "title": "Information Specialist",
+    "location": "Fort Lewis, WA",
+    "dates": "August 1997 - October 2004",
+    "description": "part of this Army group."
+  }]
+};
+
+var education = {
+  "schools": [{
+    "name": "Simon Fraser University",
+    "dates": "2009 - 2011",
+    "location": "Vancouver, BC, Canada",
+    "degree": "Msc. Finance",
+    "majors": ["Finance"],
+    "minor": ["Risk Management"],
+    "url": "www.sfu.com"
+  }, {
+    "name": "St Martin University",
+    "dates": "2003 - 2004",
+    "location": "Lacey, WA",
+    "degree": "MBA",
+    "majors": ["Operations Management"],
+    "url": "www.stmartin.edu",
+    "minor": []
+  }, {
+    "name": "Oregon state University",
+    "dates": "1998 - 2002",
+    "location": "Klamath Falls, OR",
+    "degree": "B.Sc. Computer Information Science",
+    "majors": ["Information Technology"],
+    "minor": ["Computer Science"],
+    "url": "www.oregonuni.edu",
+  }, {
+    "name": "Brunel University",
+    "dates": "1996 - 1997",
+    "location": "Uxbridge, England",
+    "degree": "HND Computer Science",
+    "majors": ["Computer Science"],
+    "url": "www.brunel.edu",
+    "minor": []
+  }],
+  "onlineCourses": [{
+    "school": "Udacity",
+    "title": "Object-Oriented Javascript",
+    "dates": "November 2014",
+    "url": "https://www.udacity.com/course/ud015"
+  }, {
+    "school": "Udacity",
+    "title": "HTML5 Canvas",
+    "dates": "November 2014",
+    "url": "https://www.udacity.com/course/ud292"
+  }, {
+    "school": "Udacity",
+    "title": "Javascript Basics",
+    "dates": "October 2014",
+    "url": "https://www.udacity.com/course/ud804"
+  }, {
+    "school": "Udacity",
+    "title": "Intro to HTML and CSS",
+    "dates": "October 2014",
+    "url": "https://www.udacity.com/course/ud304"
+  }, {
+    "school": "Udacity",
+    "title": "Web Development",
+    "dates": "August 2014",
+    "url": "https://www.udacity.com/course/cs253"
+  }]
+};
+
+var projects = {
+  "projects": [{
+    "title": "Bragout",
+    "dates": "September 2015 - March 2016",
+    "description": "Created an online game using HTML5 Canvas as part of Udacity's Front-End Web Developer " +
+      "Nanodegree.",
+    "images": ["images/bragout.png"],
+    "url": "www.bragout.com"
+  }, {
+    "title": "Online Portfolio",
+    "dates": "November 2016",
+    "description": "Created an online portfolio of work as part of Udacity's Front-End Web Developer " +
+      "Nanodegree.",
+    "images": ["images/resume.png"],
+    "url": "www.bragout.com"
+  }, {
+    "title": "Udacity's Blog Project",
+    "dates": "October 2016 - November 2016",
+    "description": "Created an online blog for Udacity's Web Development Course.",
+    "images": ["images/blog.png"],
+    "url": "www.bragout.com"
+  }]
+};
+
+bio.display = function() {
+
+  var formattedName = HTMLheaderName.replace("%data%", bio.name);
+  var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+  var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
+  var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+  var formattedContactInfo = [];
+  formattedContactInfo.push(HTMLemail.replace("%data%", bio.contacts.email));
+  formattedContactInfo.push(HTMLgithub.replace("%data%", bio.contacts.github));
+  formattedContactInfo.push(HTMLlocation.replace("%data%", bio.contacts.location));
+  formattedContactInfo.push(HTMLmobile.replace("%data%", bio.contacts.mobile));
+  formattedContactInfo.push(HTMLLinkedIN.replace("%data%", bio.contacts.LinkedIN));
+
+  $("#header").prepend(formattedRole);
+  $("#header").prepend(formattedName);
+  $("#header").append(formattedBioPic);
+  $("#header").append(formattedWelcomeMsg);
+
+
+  if (bio.skills.length > 0) {
+    $("#header").append(HTMLskillsStart);
+
+    for (var i = 0; i < bio.skills.length; i++) {
+      $("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
+    }
+  }
+
+  for (var j in formattedContactInfo) {
+    $("#topContacts").append(formattedContactInfo[j]);
+    $("#footerContacts").append(formattedContactInfo[j]);
+  }
+};
+bio.display();
+
+work.display = function() {
+
+  if (work.jobs.length > 0) {
+
+    $("#workExperience").append(HTMLworkStart);
+
+    for (var i = 0; i < work.jobs.length; i++) {
+      var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
+      var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
+      var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[i].location);
+      var formattedDatesWorked = HTMLworkDates.replace("%data%", work.jobs[i].dates);
+      var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
+
+      var formattedEmployerWorkTitle = formattedEmployer + formattedWorkTitle;
+
+      $(".work-entry:last").append(formattedEmployerWorkTitle);
+      $(".work-entry:last").append(formattedWorkLocation);
+      $(".work-entry:last").append(formattedDatesWorked);
+      $(".work-entry:last").append(formattedWorkDescription);
+    }
+
+  }
+
+};
+
+work.display();
+
+
+projects.display = function() {
+  if (projects.projects.length > 0) {
+    for (var i = 0; i < projects.projects.length; i++) {
+      $("#projects").append(HTMLprojectStart);
+
+      var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title).replace("#", projects.projects[i].url);
+      var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
+      var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
+
+      $(".project-entry:last").append(formattedProjectTitle);
+      $(".project-entry:last").append(formattedProjectDates);
+      $(".project-entry:last").append(formattedProjectDescription);
+
+      for (var img in projects.projects[i].images) {
+        var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[i].images[img]);
+        $(".project-entry:last").append(formattedProjectImage);
+      }
+
+
+    }
+  }
+};
+
+projects.display();
+
+
+education.display = function() {
+  if (education.schools.length > 0 || education.onlineCourses.length > 0) {
+    for (var i = 0; i < education.schools.length; i++) {
+      $("#education").append(HTMLschoolStart);
+
+      var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].name).replace("#", education.schools[i].url);
+      var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+      var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
+      var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
+      $(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
+      $(".education-entry:last").append(formattedSchoolDates);
+      $(".education-entry:last").append(formattedSchoolLocation);
+
+      //loop through majors
+      for (var m = 0; m < education.schools[i].majors.length; m++) {
+        var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].majors[m]);
+        $(".education-entry:last").append(formattedSchoolMajor);
+      }
+
+      // loop through minors
+      for (var n = 0; n < education.schools[i].minor.length; n++) {
+        var formattedSchoolMinor = HTMLschoolMinor.replace("%data%", education.schools[i].minor[n]);
+        $(".education-entry:last").append(formattedSchoolMinor);
+      }
+
+    }
+
+    if (education.onlineCourses.length > 0) {
+      $("#education").append(HTMLonlineClasses);
+      for (var j = 0; j < education.onlineCourses.length; j++) {
+        $("#education").append(HTMLschoolStart);
+        var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title).replace("#", education.onlineCourses[i].url);
+        var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
+        var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].dates);
+        var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url).replace("#", education.onlineCourses[i].url);
+
+        $(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+        $(".education-entry:last").append(formattedOnlineDates);
+        $(".education-entry:last").append(formattedOnlineURL);
+      }
+    }
+
+  }
+};
+
+education.display();
+$("#mapDiv").append(googleMap);
+
+/**
+ * Skills Chart
+ */
+$(function() {
+  $('#container').highcharts({
+    chart: {
+      type: 'scatter',
+      zoomType: 'xy'
+    },
+    title: {
+      text: 'Skills'
+    },
+    xAxis: {
+      title: {
+        enabled: true,
+        text: 'Used (year)'
+      },
+      startOnTick: true,
+      endOnTick: true,
+      showLastLabel: true
+    },
+    yAxis: {
+      title: {
+        text: 'Duration (months)'
+      }
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'left',
+      verticalAlign: 'top',
+      x: 250,
+      y: 0,
+      floating: true,
+      backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
+      borderWidth: 1
+    },
+    plotOptions: {
+      scatter: {
+        marker: {
+          radius: 5,
+          states: {
+            hover: {
+              enabled: true,
+              lineColor: 'rgb(100,100,100)'
+            }
+          }
+        },
+        states: {
+          hover: {
+            marker: {
+              enabled: false
+            }
+          }
+        },
+        tooltip: {
+          headerFormat: '<b>{series.name}</b><br>',
+          pointFormat: 'Used in {point.x} for {point.y} month(s)'
+        }
+      }
+    },
+    series: [{
+        name: 'JavaScript',
+        color: 'rgba(223, 83, 83, .5)',
+        data: [
+          [2016, 2],
+          [2013, 1],
+          [2007, 2]
+        ]
+      }, {
+        name: 'ruby sinatra',
+        color: 'rgba(119, 152, 191, .5)',
+        data: [
+          [2014, 1],
+          [2013, 1],
+          [2012, 1],
+          [2011, 1]
+        ]
+      }, {
+        name: 'ruby',
+        color: 'rgba(119, 152, 80, .5)',
+        data: [
+          [2016, 2]
+        ]
+      }, {
+        name: 'jQuery',
+        color: 'rgba(170, 162, 30, .5)',
+        data: [
+          [2016, 2]
+        ]
+      }, {
+        name: 'web design',
+        color: 'rgba(0, 102, 0, .5)',
+        data: [
+          [2016, 2]
+        ]
+      },
+
+      {
+        name: 'HTML/CSS',
+        color: 'rgba(153, 51, 0, .5)',
+        data: [
+          [2016, 2]
+        ]
+      }, {
+        name: 'Web Development',
+        color: 'rgba(0, 153, 102, .5)',
+        data: [
+          [2016, 2]
+        ]
+      }, {
+        name: 'Project Management',
+        color: 'rgba(0, 102, 51, .5)',
+        data: [
+          [2014, 12],
+          [2013, 12],
+          [2012, 12],
+          [2011, 4]
+        ]
+      }, {
+        name: 'Product Management',
+        color: 'rgba(204, 102, 0, .5)',
+        data: [
+          [2015, 12],
+          [2016, 11]
+        ]
+      }, {
+        name: 'Ops Management',
+        color: 'rgba(210, 200, 0, .5)',
+        data: [
+          [2011, 3],
+          [2013, 1],
+          [2012, 1],
+          [2014, 1]
+        ]
+      }
+    ]
+  });
+});
